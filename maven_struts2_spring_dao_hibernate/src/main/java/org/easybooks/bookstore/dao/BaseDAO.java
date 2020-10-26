107583870384617abf988cf0df0b20472c080a7a
pluto <365783224@qq.com>
2020-10-26 15:22:51 +0800

    补全了程序代码

---
+++ maven_struts2_spring_dao_hibernate/src/main/java/org/easybooks/bookstore/dao/BaseDAO.java
@@ -0,0 +1,24 @@
+package org.easybooks.bookstore.dao;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+public class BaseDAO {
+
+	private SessionFactory sessionFactory;
+
+	public SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+	
+	public Session getSession() {
+		
+		return sessionFactory.openSession();
+		
+	}
+	
+}
