39dcbd8372ae49c3921b1e3d73252435979260a8
pluto <365783224@qq.com>
2020-10-29 13:51:33 +0800

    新建了jsp_struts2_spring_service_dao_hibernate工程

---
+++ jsp_struts2_spring_service_dao_hibernate/src/main/java/org/easybooks/bookstore/dao/impl/UserDAO.java
@@ -0,0 +1,31 @@
+package org.easybooks.bookstore.dao.impl;
+
+import java.util.List;
+
+import org.easybooks.bookstore.dao.BaseDAO;
+import org.easybooks.bookstore.dao.IUserDAO;
+import org.easybooks.bookstore.vo.User;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+
+public class UserDAO extends BaseDAO implements IUserDAO {
+
+	@Override
+	public User validate(User user) {
+		// TODO Auto-generated method stub
+		String sql = "from User u where u.userName = ?0 and u.passWord = ?1";
+		Session session = getSession();
+		Query<User> query = session.createQuery(sql,User.class)
+				.setParameter(0, user.getUserName())
+				.setParameter(1, user.getPassWord())
+				.setMaxResults(1);
+		List<User> users=query.getResultList();
+		User exsitUser=null;
+		if (users.size() != 0) {
+			exsitUser = users.get(0);
+		}
+		session.close();
+		return exsitUser;
+	}
+
+}
